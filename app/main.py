92b8362699de91f8ed3ca1bfd19858f4e0e949a0
Artur Bertin <arturbertin2006@gmail.com>
2025-12-11 02:18:03 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -40,8 +40,3 @@ class Dog(Animal):
 
 def feed_animals(animals: list) -> int:
     return sum(animal.feed() for animal in animals)
-
-cat = Cat("Cat", False)
-lion = Animal("Lion", 25, True)
-dog = Dog("Dog")
-feed_animals([cat, lion, dog]) #32
